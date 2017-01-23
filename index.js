f1d5636c1ab83fb375a9d20670fecf42862266c9
Jannis R <mail@jannisr.de>
2017-01-23 12:35:38 +0100

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict'
 
-// todo
+const streak = (threshold = 1000, total = 0) => {
+	let firstT = 0, lastT = 0, streak = false
+
+	const check = () => {
+		const now = Date.now()
+
+		const withinThreshold = (now - lastT) <= threshold
+		const withinTotal = total > 0 ? (now - firstT) <= total : true
+		const first = !(withinThreshold && withinTotal)
+
+		if (!streak || first) {
+			streak = true
+			firstT = now
+			lastT = now
+			return true
+		} else return false
+	}
+	return check
+}
+
+module.exports = streak
